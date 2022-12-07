08d68647ff7d129ff96b78f2ada816e5281746e7
Toms Prūsis <atlas190028@gmail.com>
2022-12-07 03:27:36 +0200

    minor fixes

--- src/main/resources/db/changelog/changes/0001-create-student.sql
+++ src/main/resources/db/changelog/changes/0001-create-student.sql
@@ -7,6 +7,5 @@ CREATE TABLE student
     id             INT AUTO_INCREMENT,
     first_name   VARCHAR(255) NOT NULL,
     last_name     VARCHAR(255) NOT NULL,
-    --course_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
